19ce3e08f45c09c71f309fb52fe9fa05d905f3ca
Ross <saltlifegoodlife@gmail.com>
2019-12-11 14:23:15 -0600

    finished set 3

--- ChallengeSets/ChallengeSet03.cs
+++ ChallengeSets/ChallengeSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengeSets
 {
@@ -7,51 +8,96 @@ namespace ChallengeSets
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            foreach (var item in vals)
+            int num1 = 0;
+            foreach (var value in vals)
             {
+                if (value == false)
+                {
+                    num1++;
+                }
 
             }
-            return false;
+            if (num1 > 0)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 != 0)
+                {
+                    sum = sum + num;
+                }
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            if (password.Any(char.IsDigit) && password.Any(char.IsUpper) && password.Any(char.IsLower))
+            {
+                return true;
+            }
+            else return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
 
-            throw new NotImplementedException();
+            string one = val.Substring(val.Length - 1);
+            return Convert.ToChar(one);
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+
+            else return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            List<int> odds = new List<int>();
+            for (int i = 1; i < 100; i += 2)
+            {
+                odds.Add(i);
+            }
+            return odds.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+            
         }
     }
 }
